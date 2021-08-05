92473e27a88d2957487b0025c4edfac5ec9d9d8b
VladeCV <vladimircabrecabri@gmail.com>
2021-08-04 22:24:01 -0700

    First commit

---
+++ components/Footer.js
@@ -0,0 +1,44 @@
+function Footer() {
+  return (
+    <div
+      className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100
+    text-gray-600"
+    >
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accesibility</p>
+        <p>Not a real site</p>
+        <p>CLONE!!</p>
+        <p>Referrals Accepted</p>
+        <p>LPZ</p>
+        <p>Frontline Stays</p>
+        <p>Gift Cards</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>CVCF</p>
+        <p>Presents</p>
+        <p>Airbnb Clone</p>
+        <p>Resource Center</p>
+        <p>Commnuity Center</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Our COVID-19 Response</p>
+        <p>Help Center</p>
+        <p>Cancellation Options</p>
+        <p>Trust & Safety</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
